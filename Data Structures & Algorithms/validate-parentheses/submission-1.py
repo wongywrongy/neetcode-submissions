1f031067e9784fbc2d0d684e8ebe22262f91d25e
misogyu <51940164+wongywrongy@users.noreply.github.com>
2026-09-03 09:58:55 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closers = {')' : '(', ']' : '[', '}' : '{'}
+
+        for c in s:
+            if c in closers:
+                if not stack or stack.pop() != closers[c]:
+                    return False
+            else:
+                stack.append(c)
+            
+        return not stack
